561734bf61d9b77b6d17557a7befa9dbdf2c460d
Ye.S <small_black_luo@163.com>
2023-04-15 00:21:26 +0800

    2023-04-14-01-前端页面拆解完成-TODO后端模型设计

--- WuTongShu/details/urls.py
+++ WuTongShu/details/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # 在URL中分隔单词，应该尽可能使用连字符"-"
     path('brand-activities/', views.brand_details, name='brand_details'),
+    path('departments/', views.department_details, name='department_details'),
 ]
--- WuTongShu/details/views.py
+++ WuTongShu/details/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def brand_details(request):
     # render introduction pages for different student-work brands
     return render(request, "details/brand_details.html")
+
+
+def department_details(request):
+    # render introduction pages for different administrative departments
+    return render(request, "details/department_details.html")
